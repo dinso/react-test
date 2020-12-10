68f6b193141d943a14a445a816631f658665df94
dinso <write.dinso@gmail.com>
2020-12-10 21:04:57 +0530

    integrated firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/database'
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC7UN2ZRlaZplyHBYFMmNTjaLQvatIVkCk",
+  authDomain: "react-test-b8229.firebaseapp.com",
+  databaseURL: "https://react-test-b8229-default-rtdb.firebaseio.com",
+  projectId: "react-test-b8229",
+  storageBucket: "react-test-b8229.appspot.com",
+  messagingSenderId: "688169050693",
+  appId: "1:688169050693:web:8ce12acf1b18a0607ab92c",
+  measurementId: "G-YWENXN6WW6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Get a reference to the database service
+var database = firebase.database();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
